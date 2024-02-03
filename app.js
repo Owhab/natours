5e4e1459541217feaef3d24e4bee83572b34dcb3
Abdul Owhab <owhab.cse@gmail.com>
2024-02-04 03:01:38 +0600

    configured node express app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("Natours app listen on port: ", port);
+});
