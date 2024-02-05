b15e5bda7d3168c80a0256636f751693c704143d
Abdul Owhab <owhab.cse@gmail.com>
2024-02-06 02:21:19 +0600

    creatd home post api endpoint for test

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.post("/", (req, res) => {
+  res.status(200).send("Wow.. You can now post to this api..");
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log("Natours app listen on port: ", port);
